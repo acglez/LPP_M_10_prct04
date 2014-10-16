51b4ed0b050a9be68a619e8d41c65dff5c895d03
Adrian <acglez10@gmail.com>
2014-10-16 13:21:26 +0100

    Implementada la clase Complejo y probada en el main.

--- lib/complejo.rb
+++ lib/complejo.rb
@@ -1,23 +1,39 @@
 class Complejo
   attr_reader :real, :imaginary
+  attr_writer :real, :imaginary
   
   def initialize(real, imaginary)
-    @real, @imaginary = x, y
+    @real, @imaginary = real, imaginary
   end
   
   def to_s
-    "(#{@real},#{@imaginary})"
+    "#{@real}+#{@imaginary}i"
   end
-  
+/
   def *(value)
-    Point.new(@x * value, @y * value)
+    Complejo.new(@real * value, @imaginary * value)
   end
   
   def -@
-    Point.new(@x * -1, @y * -1)
+    Complejo.new(@real * -1, @imaginary * -1)
   end
-  
+/
   def sum (other)
     Complejo.new(@real + other.real, @imaginary + other.imaginary)
   end
+
+  def subtract (other)
+    Complejo.new(@real - other.real, @imaginary - other.imaginary)
+  end
+
+  def product (other)
+    Complejo.new(@real * other.real + @imaginary * other.imaginary, @real * other.imaginary + @imaginary * other.real)
+  end
+
+  def division (other)
+    realPart = (@real * other.real + @imaginary * other.imaginary) / (other.real**2 + other.imaginary**2)
+    imaginaryPart = (@imaginary * other.real - @real * other.imaginary) / (other.real**2 + other.imaginary**2)
+    Complejo.new(realPart,imaginaryPart)
+  end
+
 end
